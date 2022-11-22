2a29d4aa11f58c1ab2214a1cfbbfa6463cb32165
JordanLindley <jlindley94@gmail.com>
2022-11-21 20:14:23 -0500

    implement server get user info post to button

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.post('/subscriptions', (request, response) => {
+  console.log(request.body);
+  response.send(fs.readFileSync('package.json'));
+  fs.writeFileSync('subscriber.json', JSON.stringify(request.body));
+})
+
+app.listen(8080, () => {
+  console.log('But Anakin, the ray shield is still up!');
+})
